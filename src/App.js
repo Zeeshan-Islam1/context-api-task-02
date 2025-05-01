610157df579105165d4b57b3dc19cf4fead370ea
Zeeshan-Islam1 <159948421+Zeeshan-Islam1@users.noreply.github.com>
2025-05-02 02:45:35 +0500

    Normalize line endings to LF

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { CartProvider } from "./CartContext";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
+import Contact from './components/contact';
+import About from './components/About';
+import Shop from './components/shop';
+import LoginPage from './components/Login';
+import Profile from './components/profile';
+import Laptops from "../src/components/laptops";
+import Tv from "../src/components/Tv";
+import Tablet from "../src/components/tablet";
+
+import ProductPage from "./components/product";
+import ProductDetailPage from "./components/productDetail";
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Layout = () => (
+  <>
+    <Header />
+    <Outlet />
+    <Footer />
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Main />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      },
+      {
+        path: "shop",
+        element: <Shop />
+      },
+      {
+        path: "login",
+        element: <LoginPage />
+      },
+      {
+        path: "profile",
+        element: <Profile />
+      },
+      {
+        path: "laptops",
+        element: <Laptops />
+      },
+      {
+        path: "tv",
+        element: <Tv />
+      },
+      {
+        path: "tablet",
+        element: <Tablet />
+      },
+      // Add routes for Product and Product Details
+      {
+        path: "product/:id",
+        element: <ProductDetailPage />
+      },
+      {
+        path: "product",
+        element: <ProductPage />
+      },
+    ]
+  }
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
